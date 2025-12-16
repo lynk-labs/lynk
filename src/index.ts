355aa7de2bce7b3d14ca9e5caa40af3d756884fc
lynk-labs <194120946+lynk-labs@users.noreply.github.com>
2025-12-16 14:32:59 +0900

    feat(entry): export public API barrel file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * LYNK Protocol - React SDK
+ * @packageDocumentation
+ * @module @lynk-protocol/react
+ */
+
+export * from './provider';
+export * from './useLynk';
+export * from './types';
+export * from './utils/storage';
+export * from './utils/reconnect';
